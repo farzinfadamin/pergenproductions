57ed4fdc5b91986ccd6ce4e63a25bee95582d1ff
Farzin Fadamin <ffadamin@gmail.com>
2013-08-02 23:32:34 +0430

    Change readme style

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 Pergen Productions Official Website Designed and implemented by Mexo Inc.
 =================
 
-#Git Remote Reference
+### Git Remote Reference
+---------
 Link: [http://gitref.org/remotes/](http://gitref.org/remotes/)
 
-# reset the index to the desired tree
+### reset the index to the desired tree
+---------
 git reset 56e05fced
 
-# move the branch pointer back to the previous HEAD
+### move the branch pointer back to the previous HEAD
+---------
 git reset --soft HEAD@{1}
-
 git commit -m "Revert to 56e05fced"
 
-# Update working copy to reflect the new commit
+### Update working copy to reflect the new commit
+---------
 git reset --hard
--- templates_c/5648126bf84d5e7c687cbeeeb198786c586ae950.file.header.tpl.html.php
+++ templates_c/5648126bf84d5e7c687cbeeeb198786c586ae950.file.header.tpl.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.0.6, created on 2013-08-02 04:28:05
+<?php /* Smarty version Smarty-3.0.6, created on 2013-08-02 14:00:18
          compiled from "D:\wamp\www\GitHub\pergenproductions/templates/includes/header.tpl.html" */ ?>
 <?php /*%%SmartyHeaderCode:2827551fb6d95c77de4-72637308%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_smarty_tpl->decodeProperties(array (
     '5648126bf84d5e7c687cbeeeb198786c586ae950' => 
     array (
       0 => 'D:\\wamp\\www\\GitHub\\pergenproductions/templates/includes/header.tpl.html',
-      1 => 1375431944,
+      1 => 1375458361,
       2 => 'file',
     ),
   ),
@@ -22,19 +22,19 @@ $_smarty_tpl->decodeProperties(array (
 "></a></h1>
 			<nav id="main-menu">
 				<ul>
-					<li><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['homepage'];?>
+					<li <?php if ($_smarty_tpl->getVariable('action')->value=="homepage"){?>class="active"<?php }?>><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['homepage'];?>
 ">Home</a></li>
-					<li><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['services'];?>
+					<li <?php if ($_smarty_tpl->getVariable('action')->value=="services"){?>class="active"<?php }?>><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['services'];?>
 ">Services</a></li>
-					<li><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['portfolio'];?>
+					<li <?php if ($_smarty_tpl->getVariable('action')->value=="portfolio"){?>class="active"<?php }?>><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['portfolio'];?>
 ">Portfolio</a></li>
-					<li><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['testimonials'];?>
+					<li <?php if ($_smarty_tpl->getVariable('action')->value=="testimonials"){?>class="active"<?php }?>><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['testimonials'];?>
 ">Testimonials</a></li>
-					<li><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['events'];?>
+					<li <?php if ($_smarty_tpl->getVariable('action')->value=="events"){?>class="active"<?php }?>><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['events'];?>
 ">Events</a></li>
-					<li><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['about'];?>
+					<li <?php if ($_smarty_tpl->getVariable('action')->value=="about"){?>class="active"<?php }?>><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['about'];?>
 ">About us</a></li>
-					<li><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['contact'];?>
+					<li <?php if ($_smarty_tpl->getVariable('action')->value=="contact"){?>class="active"<?php }?>><a href="<?php echo $_smarty_tpl->getVariable('SiteData')->value['urls']['site']['contact'];?>
 ">Contact us</a></li>
 				</ul>
 			</nav>
