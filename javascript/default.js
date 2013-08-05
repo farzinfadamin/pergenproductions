4ff165231ea470185d6cc7fdc303a0c8ec3c336e
Farzin Fadamin <ffadamin@gmail.com>
2013-08-05 23:38:32 +0430

    New changes for portfolio and services

--- javascript/default.js
+++ javascript/default.js
@@ -1,30 +1,4 @@
 $(document).ready(function(){
 	
-	enableFancyBox();
 
 });
-
-function enableFancyBox(){
-
-	$('a[data-fancybox-group]').fancybox({
-		padding 		: 0,
-		openEffect		: 'elastic',
-		openOpacity 	: 'true',
-		helpers	: {
-			title	: {
-				type: 'outside'
-			},
-			overlay	: {
-				opacity : 0.8,
-				css : {
-					'background' : 'rgba(0,0,0,0.8)'
-				}
-			},
-			thumbs	: {
-				width	: 80,
-				height	: 50
-			},
-			media : {}
-		}
-	});
-}
