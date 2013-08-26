eb581d5f9ece7cd65adea312c04a3bf7e95f170e
Farzin Fadamin <ffadamin@gmail.com>
2013-08-26 15:48:06 +0430

    Ashkan's changes v3
    http://goo.gl/JB4Fpu

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -11,5 +11,32 @@ $(document).ready(function () {
 	$("#slide-show").tinycarousel({ interval : true, intervaltime: 5000});
 	$("#testimonial-section").tinycarousel({ interval : true, intervaltime: 5000});
 	$("#portfolio-section").tinycarousel({ interval : false, intervaltime: 5000});
+	
+	enableFancyBox();
 
 });
+
+function enableFancyBox(){
+
+	$('a[data-fancybox-group]').fancybox({
+		padding 		: 0,
+		openEffect		: 'elastic',
+		openOpacity 	: 'true',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			overlay	: {
+				opacity : 0.8,
+				css : {
+					'background' : 'rgba(0,0,0,0.8)'
+				}
+			},
+			thumbs	: {
+				width	: 80,
+				height	: 50
+			},
+			media : {}
+		}
+	});
+}
