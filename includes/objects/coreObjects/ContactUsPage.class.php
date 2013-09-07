52d2c6fe3673a3ad611d0f86713ebb3bd0fddb73
Farzin Fadamin <ffadamin@gmail.com>
2013-09-08 02:00:35 +0430

    back to previous email address

--- includes/objects/coreObjects/ContactUsPage.class.php
+++ includes/objects/coreObjects/ContactUsPage.class.php
@@ -14,8 +14,7 @@ class ContactUsPage {
 	private static $emailTemplate;
 	private static $userEmailTemplate;
 	private static $emailTitle = 'The PerGen Productions Website Contact Us Form';
-	private static $to = 'ffadamin@gmail.com';
-	//arahimza@yahoo.com
+	private static $to = 'arahimza@yahoo.com';
 	private static $cc = 'pedramphp@gmail.com';
 	private static $subject = "New Message: The PerGen Productions Website Contact Us Form";
 	private static $emailLogo;
