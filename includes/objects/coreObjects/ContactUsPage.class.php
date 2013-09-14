516c498c45ae9c9e5176e42de48d8375b9825d03
Farzin Fadamin <ffadamin@gmail.com>
2013-09-14 10:37:35 +0430

    another test

--- includes/objects/coreObjects/ContactUsPage.class.php
+++ includes/objects/coreObjects/ContactUsPage.class.php
@@ -14,9 +14,9 @@ class ContactUsPage {
 	private static $emailTemplate;
 	private static $userEmailTemplate;
 	private static $emailTitle = 'The PerGen Productions Website Contact Us Form';
-	private static $to = 'ffadamin@gmail.com,farzin.fadamin@mexo.co';
-	//arahimza@yahoo.com,info@pergenproductions.com
+	private static $to = 'ffadamin@gmail.com';
 	private static $cc = 'pedramphp@gmail.com';
+	//info@pergenproductions.com  arahimza@yahoo.com
 	private static $subject = "New Message: The PerGen Productions Website Contact Us Form";
 	private static $emailLogo;
 	private static $bgDate;
@@ -88,7 +88,7 @@ class ContactUsPage {
 		
 		
 		$headers = 'From: '.self::$emailTitle.' <'.$this->emailAddress.'> ' . "\r\n";
-		//$headers .- 'CC:' . self::$cc . "\r\n";
+		$headers .- 'CC:' . self::$cc . "\r\n";
 		$headers .= 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 		
